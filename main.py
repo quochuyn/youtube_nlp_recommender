491a01f47d2524ac97e881fc61c4eef0cc7540df
root <krishch@umich.edu>
2023-06-06 21:18:01 +0000

    d

--- main.py
+++ main.py
@@ -1,38 +1,11 @@
-from dataservice.query import run_query
+import streamlit as st
 
-dataframe = None
+# from dataservice.query import run_query
+from components.sidebar import sidebar
 
-st.title("""
-Netflix Recommendation System
-This is an Content Based Recommender System made on implicit ratings :smile:.
- """)
+st.set_page_config(layout="wide")
 
-st.text("")
-st.text("")
-st.text("")
-st.text("")
+st.header("Custom tab component for on-hover navigation bar")
+st.markdown('<style>' + open('./components/style.css').read() + '</style>', unsafe_allow_html=True)
 
-session.options = st.multiselect(label="Select Movies", options=movies)
-
-st.text("")
-st.text("")
-
-session.slider_count = st.slider(label="movie_count", min_value=5, max_value=50)
-
-st.text("")
-st.text("")
-
-buffer1, col1, buffer2 = st.columns([1.45, 1, 1])
-
-is_clicked = col1.button(label="Recommend")
-
-if is_clicked:
-    dataframe = recommend_table(session.options, movie_count=session.slider_count, tfidf_data=tfidf)
-
-st.text("")
-st.text("")
-st.text("")
-st.text("")
-
-if dataframe is not None:
-    st.table(dataframe)
+sidebar()
