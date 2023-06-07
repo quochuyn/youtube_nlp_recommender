77411c1937b3e684e2b6d1b86cb39522ac1c2c9e
root <krishch@umich.edu>
2023-06-07 01:03:15 +0000

    d

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-import streamlit as st
+#!/usr/local/bin/python
 
+import streamlit as st
+import psycopg2
 # from dataservice.query import run_query
 from components.sidebar import sidebar
+from dataservice.load_images import load_images
 
-st.set_page_config(layout="wide")
 
-st.header("Custom tab component for on-hover navigation bar")
-st.markdown('<style>' + open('./components/style.css').read() + '</style>', unsafe_allow_html=True)
+def init_connection():
+    secrets='../.streamlit/secrets.toml'
+    return psycopg2.connect(**st.secrets["postgres"])
 
-tabs = sidebar()
+conn = init_connection()
 
-if tabs == 'Dashboard':
-    input_terms = st.text_input("Enter keywords")
-    
+load_images(conn)
+
+# st.set_page_config(layout="wide")
+
+# st.header("Custom tab component for on-hover navigation bar")
+# st.markdown('<style>' + open('./components/style.css').read() + '</style>', unsafe_allow_html=True)
+
+# tabs = sidebar()
+
+# if tabs == 'Dashboard':
+#     input_terms = st.text_input("Enter keywords")
