efc2b39cef147a2497a1c39e6c5e0c2ebc1dc8a6
root <krishch@umich.edu>
2023-06-07 02:19:05 +0000

    d

--- dataservice/load_images.py
+++ dataservice/load_images.py
@@ -4,6 +4,7 @@ import psycopg2
 import streamlit as st
 from os.path import isfile, join
 from os import listdir
+import base64
 
 def load_images(conn):
     file_path = "/images/"
@@ -11,10 +12,25 @@ def load_images(conn):
     for img_file in file_list:
         with open(file_path + img_file, "rb") as image:
             f = image.read()
-            img_bytes = bytearray(f)
-            print(img_bytes[0])
+            img_bytes = base64.b64encode(f)
+            print("inserting image from file {}".format(img_file))
             with conn.cursor() as cur:
                 video_id = img_file.split('.')[0]
                 cur.execute("insert into video_topics values ('" + video_id + "', ARRAY['stremlit',  'education'], 'https://www.youtube.com/watch?v=" + \
                             video_id + "'" + ')')
-                cur.execute("insert into video_thumbnails values('" + video_id + "', " + img_bytes + ")")
+                #cur.execute("insert into video_thumbnails values('" + video_id + "', " + img_bytes + ")")
+                cur.execute('''INSERT INTO video_thumbnails VALUES (%s, %s)''', (video_id, img_bytes))
+    conn.commit()
+
+def fetch_images(conn):
+    with conn.cursor() as cur:
+        cur.execute('''SELECT thumbnail FROM video_thumbnails''')
+        # cur.execute('''SELECT video_id FROM video_topics''')
+
+        #print(cur.fetchone())
+        mview = cur.fetchone()
+        print(type(mview[0]))
+        #print(mview.tobytes())
+        #new_bin_data = bytes(mview)
+        stored_img = base64.b64decode(mview[0])
+        return stored_img
--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import streamlit as st
 import psycopg2
 # from dataservice.query import run_query
 from components.sidebar import sidebar
-from dataservice.load_images import load_images
+from dataservice.load_images import load_images, fetch_images
+from io import BytesIO
 
 
 def init_connection():
-    secrets='../.streamlit/secrets.toml'
     return psycopg2.connect(**st.secrets["postgres"])
 
 conn = init_connection()
 
-load_images(conn)
+# load_images(conn)
 
 # st.set_page_config(layout="wide")
 
@@ -24,3 +24,8 @@ load_images(conn)
 
 # if tabs == 'Dashboard':
 #     input_terms = st.text_input("Enter keywords")
+
+image_bytes = fetch_images(conn)
+# # # Create a binary stream
+image_stream = BytesIO(image_bytes)
+st.image(image_stream, caption='My Image')
