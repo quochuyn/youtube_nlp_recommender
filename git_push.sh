43d339b1eb5f16941c03fa05c2a10d6607e600be
root <krishch@umich.edu>
2023-07-06 21:39:42 +0000

    d

--- git_push.sh
+++ git_push.sh
@@ -1,3 +1,3 @@
 git config --global user.email "krishch@umich.edu"
-git add *;git status;git add *;git commit -am "d";git push  https://krishch72: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@github.com/krishch72/youtube_recommender_app.git;git status
+git add *;git status;git add *;git commit -am "d";git push  https://krishch72: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@github.com/krishch72/youtube_recommender_app.git;git status
 #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
