f34779146717ea2ea4d9f3479697485763eada6d
Gabriel Alon <gabrielalon257@gmail.com>
2023-07-08 21:11:17 -0700

    update simple filter description

--- machine_learning/simple_filter.py
+++ machine_learning/simple_filter.py
@@ -2,6 +2,8 @@
 #this will be a baseline approach of using a simple filter to recommend videos
 #of exact word matches between the filter and any video title
 
+#Pro: A video title may be long but a few word exact or close match should always be filtered out
+
 #Con: This will have limitations as sports can refer to a lot of things, but the exact
 #word may not be present in the title, transcript, tag, description, etc.
 #Con: Won't capture alternate spellings, abbreviations, etc.
