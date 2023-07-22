fb38e0437c1c813c52e76093cdccf6218fbf6142
root <krishch@umich.edu>
2023-07-22 15:37:11 +0000

    deleted git_push.sh

--- git_push.sh
+++
@@ -1,7 +0,0 @@
-git config --global user.email "krishch@umich.edu"
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied for commit message"
-    exit 1
-fi
-git add *;git status;git add *;git commit -am "$1";git push  https://krishch72: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@github.com/krishch72/youtube_recommender_app.git;git status
