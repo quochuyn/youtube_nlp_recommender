5bc8d1fc2429a0df6d5f1424c30215a4c605da7f
krishan chawla <krishch@umich.edu>
2023-08-11 10:32:17 -0700

    code cleanup

--- docker/app/start.sh
+++ docker/app/start.sh
@@ -5,6 +5,8 @@ fi
 
 
 # using git pull in start.sh to get latest code
+cd /app && /credentials/git_clone.sh
+cd ./youtube_recommender_app && git pull && pip3 install -r requirements.txt 
 cd /app/youtube_recommender_app/ && git pull && cp /credentials/secrets.toml .streamlit/ && cp /credentials/git_push.sh .
 
 if [[ $? == "0" ]]; then
