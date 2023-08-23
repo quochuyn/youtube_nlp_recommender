b9068d8e352ac269a4831775d2345fee5e4f6b9a
Quoc-Huy Nguyen <quochuyn@uci.edu>
2023-08-23 00:55:09 -0700

    Revert "ADD trubrics integration to provide feedback feature"
    This reverts commit ba4b455884499d16fd5dcd552ad1d1bb8a51059f.

--- provide_feedback.py
+++ provide_feedback.py
@@ -5,14 +5,12 @@ import pandas as pd
 
 import streamlit as st
 from sqlalchemy import create_engine
-from trubrics.integrations.streamlit import FeedbackCollector
 
 
 
 def write_feedback(conn):
     r"""
-    Provide an anonymous feedback form for users of the web app. Loosely following this guide:
-    https://blog.streamlit.io/collecting-user-feedback-on-ml-in-streamlit/
+    Provide an anonymous feedback form for users of the web app.
     """
 
     dbcredentials = st.secrets["postgres"]
@@ -26,17 +24,12 @@ def write_feedback(conn):
 
     with st.form("feedback_form"):
 
-        collector = FeedbackCollector()
+        default_text = ""
+        feedback_text = st.text_area("$Give\;Feedback$", value=default_text)
 
-        feedback_sentiment = collector.st_feedback(
-            feedback_type='thumbs'
-        )
-        feedback_text = collector.st_feedback(
-            feedback_type='issue'
-        )
+        st.write("What do you think of the web app?")
 
         submitted = st.form_submit_button("Submit")
         if submitted:
-            # TODO: write response to database
-            st.write(f"You felt: {feedback_sentiment}")
+            # write response to database
             st.write(f"You wrote: {feedback_text}")
