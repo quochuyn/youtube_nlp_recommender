d12a82a5b48a610f4046e5b462e3c0fa4c11310d
Quoc-Huy Nguyen <quochuyn@uci.edu>
2023-08-25 12:28:06 -0700

    REMOVE text feedback collector since thumb feedback collector also contains text component

--- provide_feedback.py
+++ provide_feedback.py
@@ -26,26 +26,14 @@ def write_feedback(conn):
 
     trubrics_credentials = st.secrets['trubrics']
 
-    text_collector = FeedbackCollector(
-        component_name='yt_feedback_comp_text',
-        email=trubrics_credentials['email'], # Store your Trubrics credentials in st.secrets:
-        password=trubrics_credentials['password'], # https://blog.streamlit.io/secrets-in-sharing-apps/
-    )
-
     thumb_collector = FeedbackCollector(
         component_name='yt_feedback_comp_thumbs',
         email=trubrics_credentials['email'], # Store your Trubrics credentials in st.secrets:
         password=trubrics_credentials['password'], # https://blog.streamlit.io/secrets-in-sharing-apps/
     )
 
-    text_feedback = text_collector.st_feedback(
-        feedback_type='textbox',
-        model='my_model', # TODO: what to put here?
-        open_feedback_label="What do you think of the web app?",
-    )
-
     thumbs_feedback = thumb_collector.st_feedback(
         feedback_type='thumbs',
         model='my_model', # TODO: what to put here?
-        open_feedback_label=""
+        open_feedback_label="What do you think of the web app?"
     )
